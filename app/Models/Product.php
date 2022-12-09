3d20336829e76dbd4dfa75d7e73a77692430ac52
Muneebakhan <muneebakhan430@gmail.com>
2022-12-09 16:05:43 +0500

    Updated work

---
+++ app/Models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = 'products';
+    protected $
+    // protected $fillable = ["Pname","Price","Email","Description","created_at","updated_at"];
+}
